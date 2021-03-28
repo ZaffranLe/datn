284c48d4f7e6b49028131c15d834de12a5c7b247
unknown <biprono12@gmail.com>
2021-03-28 17:30:47 +0700

    default banner

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Row, Container, Col } from "react-bootstrap";
+import { Row, Container, Col, Image } from "react-bootstrap";
+import DefaultBanner from "../../assets/img/default-banner.jpg";
 
 function Profile(props) {
     return (
         <>
-            Test
+            <Row>
+                <Col md={12} style={{ maxHeight: 400, overflowY: "hidden" }}>
+                    <Image src={DefaultBanner} style={{ width: "100%" }} />
+                </Col>
+            </Row>
         </>
     );
 }
