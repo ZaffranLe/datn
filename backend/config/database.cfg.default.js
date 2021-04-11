4b083a1d7c989e11406d30f25f72556b1e28e2c2
Zaffran Le <biprono12@gmail.com>
2021-04-12 00:45:55 +0700

    update

---
+++ backend/config/database.cfg.default.js
@@ -0,0 +1,12 @@
+const config = {
+    development: {
+        mysql: {
+            host: "localhost",
+            user: "DEFAULT_USER",
+            password: "DEFAULT_PASSWORD",
+            database: "DEFAULT_DATABASE",
+        },
+    },
+};
+
+module.exports = config;
