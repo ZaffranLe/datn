d1eb01e2d092834dedc54f212a97a00a187080ba
Zaffran <biprono12@gmail.com>
2021-07-22 20:00:45 +0700

    demn

--- backend/api/controller/socket-controller.js
+++ backend/api/controller/socket-controller.js
@@ -23,7 +23,7 @@ function handleSocket(io) {
             const msg = data.msg;
             const { idUserTo } = msg;
             if (clients[idUserTo]) {
-                io.to(clients[idUserTo].emit("receive-msg", { msg }));
+                io.to(clients[idUserTo]).emit("receive-msg", { msg });
             }
         });
     });
