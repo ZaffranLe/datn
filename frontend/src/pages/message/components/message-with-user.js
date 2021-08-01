ab8c08ffcd9dd80170f59bcf9f68f0b798bef514
PlayerNguyen <daudaua3782@gmail.com>
2021-08-01 10:45:07 +0700

    Message UI fix (overflow)

--- frontend/src/pages/message/components/message-with-user.js
+++ frontend/src/pages/message/components/message-with-user.js
@@ -13,193 +13,192 @@ import { Link } from "react-router-dom";
 import moment from "moment";
 
 function MessageWithUser({ messageGroups }) {
-    const socket = useContext(SocketContext);
-    const { currentUser, isUserInfoLoading } = useSelector((state) => state.message);
+  const socket = useContext(SocketContext);
+  const { currentUser, isUserInfoLoading } = useSelector(
+    (state) => state.message
+  );
 
-    const [selfInfo, setSelfInfo] = useState({
-        avatar: null,
-        firstName: "User",
-        lastName: "Soulatte",
-    });
+  const [selfInfo, setSelfInfo] = useState({
+    avatar: null,
+    firstName: "User",
+    lastName: "Soulatte",
+  });
 
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        const userInfo = getUserInfoFromToken();
-        setSelfInfo(userInfo);
-    }, []);
+  useEffect(() => {
+    const userInfo = getUserInfoFromToken();
+    setSelfInfo(userInfo);
+  }, []);
 
-    useEffect(() => {
-        if (messageGroups.length > 0) {
-            const chatbox = document.getElementById("chatbox");
-            chatbox.scrollTop = chatbox.scrollHeight;
-        }
-    }, [messageGroups]);
+  useEffect(() => {
+    if (messageGroups.length > 0) {
+      const chatbox = document.getElementById("chatbox");
+      chatbox.scrollTop = chatbox.scrollHeight;
+    }
+  }, [messageGroups]);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleKeyUp = (e) => {
-        const ENTER = "Enter";
-        if (e.key === ENTER && !e.shiftKey && message) {
-            console.log(message);
-            console.log(message.trim());
-            const info = {
-                idUserTo: currentUser.id,
-                content: message.trim(),
-                image: null,
-                createdAt: new Date(),
-            };
-            dispatch(messageActions.sendMessage(info, socket));
-            setMessage("");
-        }
-    };
+  const handleKeyUp = (e) => {
+    const ENTER = "Enter";
+    if (e.key === ENTER && !e.shiftKey && message) {
+      console.log(message);
+      console.log(message.trim());
+      const info = {
+        idUserTo: currentUser.id,
+        content: message.trim(),
+        image: null,
+        createdAt: new Date(),
+      };
+      dispatch(messageActions.sendMessage(info, socket));
+      setMessage("");
+    }
+  };
 
-    return (
-        <>
-            {currentUser && (
-                <Row className="bg-facebook--dark m-2 p-4 br-10 h-90">
-                    <Col md={12} className="h-100">
-                        <Row className="h-15">
-                            <Col md={12}>
-                                {isUserInfoLoading ? (
-                                    <>
-                                        <Skeleton count={1} />
-                                    </>
-                                ) : (
-                                    <div className="display--flex pb-4 mb-3 border-bottom">
-                                        <Link to={`/profile/${currentUser.slug}`}>
-                                            <LazyImage
-                                                style={{
-                                                    width: 60,
-                                                    height: 60,
-                                                    borderRadius: 60,
-                                                }}
-                                                src={
-                                                    currentUser.avatar
-                                                        ? getImageUrl(currentUser.avatar)
-                                                        : DefaultAvatar
-                                                }
-                                            />
-                                        </Link>
-                                        <div style={{ height: 60 }} className="display--table ml-2">
-                                            <span className="display--table-cell vertical-align-middle">
-                                                <Link to={`/profile/${currentUser.slug}`}>
-                                                    <big className="text-white">
-                                                        {currentUser.lastName}{" "}
-                                                        {currentUser.firstName}
-                                                    </big>
-                                                </Link>
-                                            </span>
-                                        </div>
-                                    </div>
-                                )}
-                            </Col>
-                        </Row>
-                        <Row className="h-75 overflow-auto" id="chatbox">
-                            <Col md={12} className="h-100">
-                                {messageGroups.map((_group, _idx) => (
-                                    <Row key={_idx}>
-                                        <Col
-                                            md={{
-                                                span: 4,
-                                                offset: _group.fromSelf ? 8 : 0,
-                                            }}
-                                        >
-                                            <div className="display--flex">
-                                                {!_group.fromSelf && (
-                                                    <div
-                                                        style={{ alignSelf: "flex-end" }}
-                                                        className="mr-2"
-                                                    >
-                                                        <Link to={`/profile/${currentUser.slug}`}>
-                                                            <LazyImage
-                                                                style={{
-                                                                    width: 40,
-                                                                    height: 40,
-                                                                    borderRadius: 40,
-                                                                }}
-                                                                src={
-                                                                    currentUser.avatar
-                                                                        ? getImageUrl(
-                                                                              currentUser.avatar
-                                                                          )
-                                                                        : DefaultAvatar
-                                                                }
-                                                            />
-                                                        </Link>
-                                                    </div>
-                                                )}
-                                                <div className="w-100">
-                                                    <Link
-                                                        to={`/profile/${
-                                                            _group.fromSelf
-                                                                ? selfInfo.slug
-                                                                : currentUser.slug
-                                                        }`}
-                                                    >
-                                                        <p className="text-white">
-                                                            {_group.fromSelf
-                                                                ? selfInfo.firstName
-                                                                : currentUser.firstName}
-                                                        </p>
-                                                    </Link>
-                                                    {_group.messages.map((__msg, __idx) => (
-                                                        <div
-                                                            key={__idx}
-                                                            className="p-2 msg__bg--light br-10 mt-2"
-                                                        >
-                                                            <pre className="comment text-white mb-0">
-                                                                {__msg.content}
-                                                            </pre>
-                                                        </div>
-                                                    ))}
-                                                </div>
-                                                {_group.fromSelf && (
-                                                    <div
-                                                        style={{ alignSelf: "flex-end" }}
-                                                        className="ml-2"
-                                                    >
-                                                        <LazyImage
-                                                            style={{
-                                                                width: 40,
-                                                                height: 40,
-                                                                borderRadius: 40,
-                                                            }}
-                                                            src={
-                                                                selfInfo.avatar
-                                                                    ? getImageUrl(
-                                                                          selfInfo.avatar.fileName
-                                                                      )
-                                                                    : DefaultAvatar
-                                                            }
-                                                        />
-                                                    </div>
-                                                )}
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                ))}
-                            </Col>
-                        </Row>
-                        <Row className="h-10 w-100 display--table m-0 overflow-auto">
-                            <div className="display--table-cell vertical-align-middle w-100">
-                                <FormControl
-                                    className="w-100"
-                                    type="text"
-                                    placeholder="Tin nhắn"
-                                    onChange={(e) => setMessage(e.target.value)}
-                                    onKeyUp={handleKeyUp}
-                                    as={TextareaAutosize}
-                                    maxRows={1}
-                                    value={message}
-                                />
+  return (
+    <>
+      {currentUser && (
+        <Row className="bg-facebook--dark m-2 p-4 br-10 h-90 message--box__wrapper">
+          <Col md={12} className="h-100 message--box">
+            <Row className="">
+              <Col md={12}>
+                {isUserInfoLoading ? (
+                  <>
+                    <Skeleton count={1} />
+                  </>
+                ) : (
+                  <div className="display--flex pb-4 mb-3 border-bottom message-box__header">
+                    <Link to={`/profile/${currentUser.slug}`}>
+                      <LazyImage
+                        style={{
+                          width: 60,
+                          height: 60,
+                          borderRadius: 60,
+                        }}
+                        src={
+                          currentUser.avatar
+                            ? getImageUrl(currentUser.avatar)
+                            : DefaultAvatar
+                        }
+                      />
+                    </Link>
+                    <div style={{ height: 60 }} className="display--table ml-2">
+                      <span className="display--table-cell vertical-align-middle">
+                        <Link to={`/profile/${currentUser.slug}`}>
+                          <big className="text-white">
+                            {currentUser.lastName} {currentUser.firstName}
+                          </big>
+                        </Link>
+                      </span>
+                    </div>
+                  </div>
+                )}
+              </Col>
+            </Row>
+            {/* Chat box is a body of message--box */}
+            <Row className="message--box__body" id="chatbox">
+              <Col md={12} className="message-content-wrapper">
+                {messageGroups.map((_group, _idx) => (
+                  <Row key={_idx}>
+                    <Col
+                      md={{
+                        span: 4,
+                        offset: _group.fromSelf ? 8 : 0,
+                      }}
+                    >
+                      <div className="display--flex">
+                        {!_group.fromSelf && (
+                          <div
+                            style={{ alignSelf: "flex-end" }}
+                            className="mr-2"
+                          >
+                            <Link to={`/profile/${currentUser.slug}`}>
+                              <LazyImage
+                                style={{
+                                  width: 40,
+                                  height: 40,
+                                  borderRadius: 40,
+                                }}
+                                src={
+                                  currentUser.avatar
+                                    ? getImageUrl(currentUser.avatar)
+                                    : DefaultAvatar
+                                }
+                              />
+                            </Link>
+                          </div>
+                        )}
+                        <div className="w-100">
+                          <Link
+                            to={`/profile/${
+                              _group.fromSelf ? selfInfo.slug : currentUser.slug
+                            }`}
+                          >
+                            <p className="text-white">
+                              {_group.fromSelf
+                                ? selfInfo.firstName
+                                : currentUser.firstName}
+                            </p>
+                          </Link>
+                          {_group.messages.map((__msg, __idx) => (
+                            <div
+                              key={__idx}
+                              className="p-2 msg__bg--light br-10 mt-2"
+                            >
+                              <pre className="comment text-white mb-0 message-content">
+                                {__msg.content}
+                              </pre>
                             </div>
-                        </Row>
+                          ))}
+                        </div>
+                        {_group.fromSelf && (
+                          <div
+                            style={{ alignSelf: "flex-end" }}
+                            className="ml-2"
+                          >
+                            <LazyImage
+                              style={{
+                                width: 40,
+                                height: 40,
+                                borderRadius: 40,
+                              }}
+                              src={
+                                selfInfo.avatar
+                                  ? getImageUrl(selfInfo.avatar.fileName)
+                                  : DefaultAvatar
+                              }
+                            />
+                          </div>
+                        )}
+                      </div>
                     </Col>
-                </Row>
-            )}
-        </>
-    );
+                  </Row>
+                ))}
+              </Col>
+            </Row>
+            {/* Message input box */}
+            <Row className="h-10 w-100 display--table m-0 ">
+              <div className="display--table-cell vertical-align-middle w-100">
+                <FormControl
+                  className="w-100"
+                  type="text"
+                  placeholder="Tin nhắn"
+                  onChange={(e) => setMessage(e.target.value)}
+                  onKeyUp={handleKeyUp}
+                  as={TextareaAutosize}
+                  maxRows={1}
+                  value={message}
+                  // Should have a resize script or use resizable library
+                  style={{resize: "none"}}
+                />
+              </div>
+            </Row>
+          </Col>
+        </Row>
+      )}
+    </>
+  );
 }
 
 export default MessageWithUser;
--- frontend/src/pages/message/index.js
+++ frontend/src/pages/message/index.js
@@ -10,82 +10,82 @@ import "./style.scss";
 import { useHistory } from "react-router-dom";
 
 function Message(props) {
-    const [messageGroups, setMessageGroups] = useState([]);
-    const { currentUser, currentMessages, isLoading, messageList } = useSelector(
-        (state) => state.message
-    );
+  const [messageGroups, setMessageGroups] = useState([]);
+  const { currentUser, currentMessages, isLoading, messageList } = useSelector(
+    (state) => state.message
+  );
 
-    const dispatch = useDispatch();
-    const history = useHistory();
-    useEffect(() => {
-        if (currentUser) {
-            const userInfo = getUserInfoFromToken();
-            const _messageGroups = [];
-            currentMessages.forEach((_msg, _idx) => {
-                const FROM_SELF = _msg.idUserFrom === userInfo.id;
-                if (
-                    !currentMessages[_idx - 1] ||
-                    currentMessages[_idx - 1].idUserFrom !== _msg.idUserFrom
-                ) {
-                    _messageGroups.push({
-                        messages: [_msg],
-                        firstName: FROM_SELF ? userInfo.firstName : currentUser.firstName,
-                        lastName: FROM_SELF ? userInfo.lastName : currentUser.lastName,
-                        fromSelf: FROM_SELF,
-                    });
-                } else {
-                    _messageGroups[_messageGroups.length - 1].messages.push(_msg);
-                }
-            });
-            setMessageGroups(_messageGroups);
-        }
-    }, [currentMessages, currentUser]);
-
-    useEffect(() => {
-        const _slug = props.match.params.slug;
-        if (_slug) {
-            dispatch(messageActions.getAllByUserSlug(_slug));
-            dispatch(messageActions.getUserBasicInfoBySlug(_slug));
+  const dispatch = useDispatch();
+  const history = useHistory();
+  useEffect(() => {
+    if (currentUser) {
+      const userInfo = getUserInfoFromToken();
+      const _messageGroups = [];
+      currentMessages.forEach((_msg, _idx) => {
+        const FROM_SELF = _msg.idUserFrom === userInfo.id;
+        if (
+          !currentMessages[_idx - 1] ||
+          currentMessages[_idx - 1].idUserFrom !== _msg.idUserFrom
+        ) {
+          _messageGroups.push({
+            messages: [_msg],
+            firstName: FROM_SELF ? userInfo.firstName : currentUser.firstName,
+            lastName: FROM_SELF ? userInfo.lastName : currentUser.lastName,
+            fromSelf: FROM_SELF,
+          });
         } else {
-            if (messageList.length > 0) {
-                history.push(`/messages/${messageList[0].slug}`);
-            } else {
-                dispatch(messageActions.setCurrentUser(null));
-                dispatch(messageActions.setCurrentMessages([]));
-            }
+          _messageGroups[_messageGroups.length - 1].messages.push(_msg);
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.match.params.slug, dispatch]);
+      });
+      setMessageGroups(_messageGroups);
+    }
+  }, [currentMessages, currentUser]);
 
-    return (
-        <>
-            <Row className="bg-facebook--darker h-100">
-                <Col md={12} className="p-5 h-100">
-                    <Row>
-                        <Col md={12}>
-                            <span className="h1">Tin nhắn</span>
-                        </Col>
-                    </Row>
-                    <Row className="h-100">
-                        <Col md={4} className="h-100">
-                            <MessageList />
-                        </Col>
-                        <Col md={8} className="h-100">
-                            {isLoading ? (
-                                <div className="w-100 h-100 text-center display--table">
-                                    <div className="display--table-cell vertical-align-middle">
-                                        <CustomLoader />
-                                    </div>
-                                </div>
-                            ) : (
-                                <MessageWithUser messageGroups={messageGroups} />
-                            )}
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </>
-    );
+  useEffect(() => {
+    const _slug = props.match.params.slug;
+    if (_slug) {
+      dispatch(messageActions.getAllByUserSlug(_slug));
+      dispatch(messageActions.getUserBasicInfoBySlug(_slug));
+    } else {
+      if (messageList.length > 0) {
+        history.push(`/messages/${messageList[0].slug}`);
+      } else {
+        dispatch(messageActions.setCurrentUser(null));
+        dispatch(messageActions.setCurrentMessages([]));
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.match.params.slug, dispatch]);
+
+  return (
+    <>
+      <Row className="bg-facebook--darker h-100">
+        <Col md={12} className="p-5 h-100">
+          <Row>
+            <Col md={12}>
+              <span className="h1">Tin nhắn</span>
+            </Col>
+          </Row>
+          <Row className="h-100">
+            <Col md={4} className="h-100">
+              <MessageList />
+            </Col>
+            <Col md={8} className="h-100">
+              {isLoading ? (
+                <div className="w-100 h-100 text-center display--table">
+                  <div className="display--table-cell vertical-align-middle">
+                    <CustomLoader />
+                  </div>
+                </div>
+              ) : (
+                <MessageWithUser messageGroups={messageGroups} />
+              )}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Message;
